f445728153d8d6a9ac05ac83b210954b9e956fef
Peguiny <2717882L@student.gla.ac.uk>
2023-03-04 19:34:40 +0800

    Login / Page protect

--- pages/login/index.js
+++ pages/login/index.js
@@ -9,8 +9,19 @@ import {
   Link,
 } from "@chakra-ui/react";
 import Head from "next/head";
-
+import { signIn } from "next-auth/react"
+import { useState } from "react";
 function Login() {
+  const [userInfo, setUserInfo] = useState({ email: "", password: "" })
+  const handleSumit = async (e) => {
+    // validate
+    e.preventDefault()
+    const res = await signIn("credentials", { email: userInfo.email, password: userInfo.password, callbackUrl: "/admin" })
+    console.log(res)
+    if (res.ok) {
+
+    }
+  };
   return (
     <>
       <Head>
@@ -33,17 +44,33 @@ function Login() {
             <Heading>Admin Login</Heading>
           </Box>
           <Box p={4}>
-            <FormControl>
-              <FormLabel>Username</FormLabel>
-              <Input type="text" placeholder="Username" />
-            </FormControl>
-            <FormControl mt={4}>
-              <FormLabel>Password</FormLabel>
-              <Input type="password" placeholder="Password" />
-            </FormControl>
-            <Button width="full" mt={4} colorScheme="teal" as={Link} href="/admin">
-              Login
-            </Button>
+            <form onSubmit={handleSumit} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+              <label style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", marginBottom: "1rem" }}>
+                <h1 style={{ marginBottom: "0.5rem" }}>Email</h1>
+                <input
+                  value={userInfo.email}
+                  type="email"
+                  placeholder=""
+                  onChange={({ target }) =>
+                    setUserInfo({ ...userInfo, email: target.value })
+                  }
+                  style={{ padding: "0.5rem", borderRadius: "4px", border: "1px solid #ccc", width: "100%" }}
+                />
+              </label>
+              <label style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", marginBottom: "1rem" }}>
+                <h1 style={{ marginBottom: "0.5rem" }}>Password</h1>
+                <input
+                  value={userInfo.password}
+                  type="password"
+                  placeholder=""
+                  onChange={({ target }) =>
+                    setUserInfo({ ...userInfo, password: target.value })
+                  }
+                  style={{ padding: "0.5rem", borderRadius: "4px", border: "1px solid #ccc", width: "100%" }}
+                />
+              </label>
+              <input type="submit" value="Login" style={{ backgroundColor: "#0070f3", color: "#fff", border: "none", borderRadius: "4px", padding: "0.5rem 1rem", cursor: "pointer" }} />
+            </form>
           </Box>
         </Box>
       </Flex>
